dc8f2359f1c009400d71da0a4f88963762f4b635
Simeone-Holanda <simeone.10s10@gmail.com>
2021-09-14 21:52:07 -0300

    Criado o sistema de login, cadastro e controle de acesso a determinadas rotas.

---
+++ website/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+DB_NAME = 'database.db'
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'mykey_flask'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User
+
+    create_database(app)
+
+    # Estamos dizendo abaixo que toda vez que um usuario tentar acessar um endpoint 
+    # sem que esteja logado redirecionamos ele para a tela de login.
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        """ Com isso verificamos o usuario para saber se ele ja está logado aparti de uma session anterior
+            para assim redirecionarmos ele para outra pagina que não seja a de login """
+        return User.query.get(int(id))
+
+    return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Db criado')
